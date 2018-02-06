ba1cff7d9605ae44278fb5ee50957965e218abb3
ejaworska <ewel.jaworska@gmail.com>
2018-02-06 09:22:01 +0100

    Add next function as the solution to complete Day 2

--- adventofcode.py
+++ adventofcode.py
@@ -1,3 +1,5 @@
+import itertools
+
 def return_string_from_file(file_name):
     with open(file_name, "r") as f:
         line = f.readline()
@@ -50,10 +52,38 @@ def give_the_solution_day_2_1(file_name):
     checksum = sum(list_of_diff)
     return checksum
 
+def give_the_solution_day_2_2(file_name):
+    with open(file_name, "r") as f:
+        list_of_lines = f.read().splitlines()
+
+    list_of_rows = []
+    for line in list_of_lines:
+        list_of_rows.append(line.split("\t"))
+
+    for line in list_of_rows:
+        for i in range(len(line)):
+            line[i] = int(line[i])
+
+    list_of_per = []#lista zawierajća listy zawierające tuple
+    for i in range(len(list_of_rows)):
+        list_of_per.append(list(itertools.permutations(list_of_rows[i], 2)))
+
+    division_results = []
+    for list_ in list_of_per:
+        for set_ in list_:
+            if set_[0]%set_[1] == 0:
+                division_results.append(set_[0]/set_[1])
+
+    checksum = int(sum(division_results))
+    return checksum
+
+
+
 
 def main():
-    print(give_the_solution_day_1_1("day1.txt"))
-    print(give_the_solution_day_1_2("day1.txt"))
-    print(give_the_solution_day_2_1("day2.txt"))
+    #print(give_the_solution_day_1_1("day1.txt"))
+    #print(give_the_solution_day_1_2("day1.txt"))
+    #print(give_the_solution_day_2_1("day2.txt"))
+    print(give_the_solution_day_2_2("day2.txt"))
 if __name__ == '__main__':
     main()
