6cb489df03db2376629799c19b25552a0497c4db
ejaworska <ewel.jaworska@gmail.com>
2018-02-07 19:14:24 +0100

    Add docstrings and clean the code

--- adventofcode.py
+++ adventofcode.py
@@ -6,7 +6,12 @@ def return_string_from_file(file_name):
         line = line.strip("\n")
         return line
 
+
 def give_the_solution_day_1_1(file_name):
+    """The captcha requires you to review a sequence of digits (your puzzle input)
+     and find the sum of all digits that match the next digit in the list. The list
+      is circular, so the digit after the last digit is the first digit in the list."""
+
     puzzle_input = return_string_from_file(file_name)
 
     puzzle = puzzle_input + puzzle_input[0]
@@ -20,8 +25,11 @@ def give_the_solution_day_1_1(file_name):
     return sum_puzzle
 
 
-
 def give_the_solution_day_1_2(file_name):
+    """Now, instead of considering the next digit, it wants you to consider the digit
+    halfway around the circular list. If your list contains 10 items, only
+    include a digit in your sum if the digit 10/2 = 5 steps forward matches it."""
+
     puzzle_input = return_string_from_file(file_name)
 
     items_ahead = int(len(puzzle_input)/2)
@@ -35,14 +43,14 @@ def give_the_solution_day_1_2(file_name):
 
 
 def give_the_solution_day_2_1(file_name):
-    with open(file_name, "r") as f:
-        list_of_lines = f.read().splitlines()
-    for i in range(len(list_of_lines)):
-        list_of_lines[i] = list_of_lines[i].split("\t")
+    """For each row, determine the difference between the largest value and the
+    smallest value; the checksum is the sum of all of these differences."""
 
-    for line in list_of_lines:
-        for i in range(len(line)):
-            line[i] = int(line[i])
+    list_of_lines = open_file_as_list_of_lines(file_name)
+
+    split_lines_in_list(list_of_lines)
+
+    change_type_of_line_element_to_integer(list_of_lines)
 
     list_of_diff = []
     for line in list_of_lines:
@@ -54,20 +62,19 @@ def give_the_solution_day_2_1(file_name):
 
 
 def give_the_solution_day_2_2(file_name):
-    with open(file_name, "r") as f:
-        list_of_lines = f.read().splitlines()
+    """Find the only two numbers in each row where one evenly divides the other,
+    where the result of the division operation is a whole number. Find those
+    numbers on each line, divide them, and add up each line's result."""
 
-    list_of_rows = []
-    for line in list_of_lines:
-        list_of_rows.append(line.split("\t"))
+    list_of_lines = open_file_as_list_of_lines(file_name)
 
-    for line in list_of_rows:
-        for i in range(len(line)):
-            line[i] = int(line[i])
+    split_lines_in_list(list_of_lines)
+
+    change_type_of_line_element_to_integer(list_of_lines)
 
     list_of_per = []#lista zawierajća listy zawierające tuple
-    for i in range(len(list_of_rows)):
-        list_of_per.append(list(itertools.permutations(list_of_rows[i], 2)))
+    for i in range(len(list_of_lines)):
+        list_of_per.append(list(itertools.permutations(list_of_lines[i], 2)))
 
     division_results = []
     for list_ in list_of_per:
@@ -77,6 +84,19 @@ def give_the_solution_day_2_2(file_name):
 
     checksum = int(sum(division_results))
     return checksum
+
+
+def split_lines_in_list(list_of_lines):
+    for i in range(len(list_of_lines)):
+        list_of_lines[i] = list_of_lines[i].split("\t")
+    return list_of_lines
+
+
+def change_type_of_line_element_to_integer(list_of_lines):
+    for line in list_of_lines:
+        for i in range(len(line)):
+            line[i] = int(line[i])
+    return list_of_lines
 
 
 def give_the_solution_day_3_1(puzzle_input):
@@ -93,6 +113,7 @@ def give_the_solution_day_3_1(puzzle_input):
             steps = r
 
     return steps
+
 
 def give_the_solution_day_3_2(n):
     spiral = {}
@@ -135,16 +156,20 @@ def give_the_solution_day_3_2(n):
 
 
 def give_the_solution_day_4_1(file_name):
+    """A passphrase consists of a series of words (lowercase letters) separated by spaces.
+    A valid passphrase must contain no duplicate words. How many passphrases are valid?"""
 
-    list_of_rows = open_file_as_list_of_rows(file_name)
-    list_of_lists = split_rows_in_list_of_rows(list_of_rows)
+    list_of_lines = open_file_as_list_of_lines(file_name)
+    list_of_lists = split_lines_in_list_of_lines(list_of_lines)
     return check_valid_passphrases(list_of_lists)
 
 
 def give_the_solution_day_4_2(file_name):
+    """A valid passphrase must contain no two words that are anagrams of each other.
+    How many passphrases are valid?"""
 
-    list_of_rows = open_file_as_list_of_rows(file_name)
-    list_of_lists = split_rows_in_list_of_rows(list_of_rows)
+    list_of_lines = open_file_as_list_of_lines(file_name)
+    list_of_lists = split_lines_in_list_of_lines(list_of_lines)
 
     for list_ in list_of_lists:
         for i in range(len(list_)):
@@ -157,22 +182,20 @@ def give_the_solution_day_4_2(file_name):
     return check_valid_passphrases(list_of_lists)
 
 
-def open_file_as_list_of_rows(file_name):
-
+def open_file_as_list_of_lines(file_name):
     with open(file_name, "r") as my_file:
-        list_of_rows = my_file.read().splitlines()
-    return list_of_rows
+        list_of_lines = my_file.read().splitlines()
+    return list_of_lines
 
 
-def split_rows_in_list_of_rows(list_of_rows):
+def split_lines_in_list_of_lines(list_of_lines):
     list_of_lists = []
-    for row in list_of_rows:
+    for row in list_of_lines:
         list_of_lists.append(row.split(" "))
     return list_of_lists
 
 
 def check_valid_passphrases(list_of_lists):
-
     valid = 0
     for list_ in list_of_lists:
         if len(list_) == len(set(list_)):
@@ -181,37 +204,69 @@ def check_valid_passphrases(list_of_lists):
 
 
 def give_the_solution_day_5_1(file_name):
+    """The message includes a list of the offsets for each jump.
+    Jumps are relative: -1 moves to the previous instruction, and 2 skips the next one.
+    Start at the first instruction in the list.
+    The goal is to follow the jumps until one leads outside the list.
+    After each jump, the offset increases by 1."""
 
-    jump_list = open_file_as_list_of_rows(file_name)
+    offsets_list = open_file_as_list_of_lines(file_name)
 
-    for i in range(len(jump_list)):
-        jump_list[i] = int(jump_list[i])
+    for i in range(len(offsets_list)):
+        offsets_list[i] = int(offsets_list[i])
 
     jump = 0
     index = 0
-    number = jump_list[index]
+    offset = offsets_list[index]
     while True:
         try:
             jump += 1
-            index += number
-            jump_list[index - number] += 1
-            number = jump_list[index]
+            index += offset
+            offsets_list[index - offset] += 1
+            offset = offsets_list[index]
         except IndexError:
             print("The exit is reached in", jump, "jumps")
             break
 
 
+def give_the_solution_day_5_2(file_name):
+    """After each jump, if the offset was three or more, instead decrease it by 1.
+    Otherwise, increase it by 1 as before."""
+
+    offsets_list = open_file_as_list_of_lines(file_name)
+
+    for i in range(len(offsets_list)):
+        offsets_list[i] = int(offsets_list[i])
+
+    jump = 0
+    index = 0
+    offset = offsets_list[index]
+
+    while True:
+        try:
+            jump += 1
+            index += offset
+            if offsets_list[index - offset] >= 3:
+                offsets_list[index - offset] -= 1
+            else:
+                offsets_list[index - offset] += 1
+            offset = offsets_list[index]
+        except IndexError:
+            print("The exit is reached in", jump, "jumps")
+            break
+
 
 def main():
-    #print(give_the_solution_day_1_1("day1.txt"))
-    #print(give_the_solution_day_1_2("day1.txt"))
-    #print(give_the_solution_day_2_1("day2.txt"))
-    #print(give_the_solution_day_2_2("day2.txt"))
-    #print(give_the_solution_day_3_1(23))
-    #print(give_the_solution_day_3_2(361527))
-    #print(give_the_solution_day_4_1("day4.txt"))
-    #print(give_the_solution_day_4_2("day4.txt"))
+    print(give_the_solution_day_1_1("day1.txt"))
+    print(give_the_solution_day_1_2("day1.txt"))
+    print(give_the_solution_day_2_1("day2.txt"))
+    print(give_the_solution_day_2_2("day2.txt"))
+    print(give_the_solution_day_3_1(361527))
+    print(give_the_solution_day_3_2(361527))
+    print(give_the_solution_day_4_1("day4.txt"))
+    print(give_the_solution_day_4_2("day4.txt"))
     give_the_solution_day_5_1("day5.txt")
+    give_the_solution_day_5_2("day5.txt")
 
 
 if __name__ == '__main__':
